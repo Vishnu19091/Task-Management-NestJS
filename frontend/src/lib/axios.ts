6b81d8e57e79037d6dec2295e0fd7f81410fcfa6
Vishnu19091 <vishnusah368@gmail.com>
2025-08-02 15:02:45 +0530

    Test-deploy

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   // If your backend is on the same domain
 
   // TODO: Change it to your server DNS later
-  baseURL: "http://vishnu1183.auxois-ray.ts.net:3000",
+  baseURL: "https://vishnu1183.auxois-ray.ts.net:3000",
   headers: {
     "Content-Type": "application/json",
   },
